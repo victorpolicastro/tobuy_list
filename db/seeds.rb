56810aa53efe9de5d2b6ef140bcdc8c842e7b202
Victor <policastro.victor@gmail.com>
2019-03-21 09:29:33 -0300

    Created unities seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Create system configurations
 SystemConfiguration.create(key: 'SYSTEM_STATUS', value: 'ON_HOLD')
+
+# Create unities
+Unity.create(description: 'miligramas')
+Unity.create(description: 'gramas')
+Unity.create(description: 'quilogramas')
+Unity.create(description: 'unidade')
+Unity.create(description: 'cacho')
+Unity.create(description: 'pacote')
+Unity.create(description: 'embalagem')
+Unity.create(description: 'milímetro')
+Unity.create(description: 'centimetro')
+Unity.create(description: 'metro')
+Unity.create(description: 'mililitro')
+Unity.create(description: 'litro')
