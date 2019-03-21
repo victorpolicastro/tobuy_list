e8e040ae51d85d1df587ceb5199ec2c538f96ef9
Victor <policastro.victor@gmail.com>
2019-03-21 08:52:49 -0300

    Created categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name])
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :last_name, :email, :password, :current_password, :remember_me, :image) }
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :categories
   devise_for :users
 end
